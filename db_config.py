f74d1e2af1af1f825b0c323816fcb4d70cb5cec7
Juscelino Cordeiro <jfilhodev@gmail.com>
2021-03-02 06:03:37 -0300

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,8 +4,8 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
  
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'dimitri'
-app.config['MYSQL_DATABASE_PASSWORD'] = '@!@#rf'
+app.config['MYSQL_DATABASE_USER'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'ms_tipo_veiculo'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
